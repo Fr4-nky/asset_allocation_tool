b9f138f7974d7e34e26fafe3d038f4b1a3120179
silvanfrank <gurkensalat12@gmail.com>
2025-04-29 13:04:09 -0300

    rename checkbox on all-weather tab.

--- tabs/all_weather.py
+++ tabs/all_weather.py
@@ -14,8 +14,8 @@ def render(tab, asset_ts_data, sp_inflation_data, session_state):
     )
     dynamic_cutoff_date = get_dynamic_cutoff_date_from_trade_log(trade_log_df, min_assets_required)
     cutoff_date = dynamic_cutoff_date if dynamic_cutoff_date is not None else session_state.get('ma_start_date')
+    checkbox_label = f"also include trades before {cutoff_date.strftime('%Y-%m-%d')} (when at least {min_assets_required} assets are present in a regime) in aggregations and bar charts."
     pre_cutoff_checkbox_key = f"include_pre_cutoff_trades_all_weather_{hashlib.md5(str(asset_list_tab7).encode()).hexdigest()}"
-    checkbox_label = "include trades before cutoff date (for assets with later start dates) in aggregations and bar charts."
     include_pre_cutoff_trades = st.checkbox(
         checkbox_label,
         value=session_state.get(pre_cutoff_checkbox_key, False),
