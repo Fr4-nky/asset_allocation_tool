dab865946ffdb272e494e70b5358319b30b45b6e
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2025-05-29 15:09:57 +0530

    Update API_BASE_URL in configuration files to use .net domain

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-API_BASE_URL = os.getenv("API_BASE_URL",'https://www.longtermtrends.com')
+API_BASE_URL = os.getenv("API_BASE_URL",'https://www.longtermtrends.net')
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
